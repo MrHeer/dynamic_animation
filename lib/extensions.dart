97cd4441340b8701fd9ec02c1c74735177dd0486
He Linming <hlm52pk@163.com>
2023-11-07 22:13:46 +0800

    docs: fix CHANGELOG

--- lib/extensions.dart
+++ lib/extensions.dart
@@ -1,26 +1,3 @@
-import 'package:flutter/animation.dart';
-import 'package:flutter/physics.dart';
+library extensions;
 
-const SpringDescription defaultSpringDescription = SpringDescription(
-  mass: 30.0,
-  stiffness: 1.0,
-  damping: 1.0,
-);
-
-extension DynamicAnimation on AnimationController {
-  /// Dynamically-updatable animate.
-  TickerFuture dynamicAnimateWith({
-    required double target,
-    double? value,
-    double? velocity,
-    SpringDescription? springDescription,
-  }) {
-    final simulation = SpringSimulation(
-      springDescription ?? defaultSpringDescription,
-      value ?? this.value,
-      target,
-      velocity ?? this.velocity,
-    );
-    return animateWith(simulation);
-  }
-}
+export 'src/extensions.dart';
